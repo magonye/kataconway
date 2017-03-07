7538613d7fc05341d94e0e5c4d9bef73110d24db
magonye <magonye@gmail.com>
2017-03-07 23:00:10 +0100

    Rule number 4

--- src/main/java/ConwayLife.java
+++ src/main/java/ConwayLife.java
@@ -8,16 +8,16 @@ class ConwayLife {
         for (int dimension = 0; dimension < x; dimension++) {
             for (int size = 0; size < y; size++) {
                 if (glider[dimension][size]==0){
-                    result[dimension][size]=0;
+                    result[dimension][size]=findNewDeadCellStatus(glider,dimension,size);
                 } else {
-                    result[dimension][size]=findNewCellStatus(glider,dimension,size);
+                    result[dimension][size]=findNewLivingCellStatus(glider,dimension,size);
                 }
             }
         }
         return result;
     }
 
-    static int findNewCellStatus(int[][] glider,int x, int y){
+    static int findNewLivingCellStatus(int[][] glider,int x, int y){
         int newStatus = 0;
         for (int i = x-1; i <= x+1; i++) {
             for (int j = y-1; j <= y+1; j++) {
@@ -35,4 +35,21 @@ class ConwayLife {
         return ((newStatus==2 || newStatus==3)?1:0);
     }
 
+    static int findNewDeadCellStatus(int[][] glider,int x, int y){
+        int newStatus = 0;
+        for (int i = x-1; i <= x+1; i++) {
+            for (int j = y-1; j <= y+1; j++) {
+                if (i>=0 && i<glider.length){
+                    if (j>=0 && j<glider[0].length){
+                        if (i!=x || j != y){
+                            if (glider[i][j]==1){
+                                newStatus++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ((newStatus==3)?1:0);
+    }
 }
--- src/test/java/ConwayLifeShould.java
+++ src/test/java/ConwayLifeShould.java
@@ -22,7 +22,7 @@ public class ConwayLifeShould {
     satisfact_rule_number_2(){
         int[][][] gliders = {
                 {{1,0,1}, {0,1,0}, {1,0,1}},
-                {{0,0,0}, {0,0,0}, {0,0,0}}
+                {{0,1,0}, {1,0,1}, {0,1,0}}
         };
         System.out.println("Glider");
         LifeDebug.print(gliders[0]);
@@ -35,7 +35,7 @@ public class ConwayLifeShould {
     satisfact_rule_number_3(){
         int[][][] gliders = {
                 {{0,0,0}, {0,1,0}, {1,1,1}},
-                {{0,0,0}, {0,1,0}, {1,1,1}}
+                {{0,0,0}, {1,1,1}, {1,1,1}}
         };
         System.out.println("Glider");
         LifeDebug.print(gliders[0]);
@@ -44,6 +44,17 @@ public class ConwayLifeShould {
 
     }
 
+    @Test public void
+    satisfact_rule_number_4(){
+        int[][][] gliders = {
+                {{0,0,0}, {1,1,1}, {0,0,0}},
+                {{0,1,0}, {0,1,0}, {0,1,0}}
+        };
+        System.out.println("Glider");
+        LifeDebug.print(gliders[0]);
+        int[][] res = ConwayLife.getGeneration(gliders[0], 1);
+        assertTrue("Got \n" + LifeDebug.htmlize(res) + "\ninstead of\n" + LifeDebug.htmlize(gliders[1]), LifeDebug.equals(res, gliders[1]));
 
+    }
 
 }
