e63c0a5a1f431a0d05129355dc295ab5b53079c1
magonye <magonye@gmail.com>
2017-03-07 22:55:17 +0100

    Rule number 3

--- src/main/java/ConwayLife.java
+++ src/main/java/ConwayLife.java
@@ -1,7 +1,38 @@
 class ConwayLife {
     static int[][] getGeneration(int[][] glider, int i) {
-        return new int[][]{{0,0,0},
-                {0,0,0},
-                {0,0,0}};
+        int x = glider.length;
+        int y = glider[0].length;
+        int[][] result = new int[x][y];
+
+
+        for (int dimension = 0; dimension < x; dimension++) {
+            for (int size = 0; size < y; size++) {
+                if (glider[dimension][size]==0){
+                    result[dimension][size]=0;
+                } else {
+                    result[dimension][size]=findNewCellStatus(glider,dimension,size);
+                }
+            }
+        }
+        return result;
     }
+
+    static int findNewCellStatus(int[][] glider,int x, int y){
+        int newStatus = 0;
+        for (int i = x-1; i <= x+1; i++) {
+            for (int j = y-1; j <= y+1; j++) {
+                if (i>=0 && i<glider.length){
+                    if (j>=0 && j<glider[0].length){
+                        if (i!=x || j != y){
+                            if (glider[i][j]==1){
+                                newStatus++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ((newStatus==2 || newStatus==3)?1:0);
+    }
+
 }
--- src/test/java/ConwayLifeShould.java
+++ src/test/java/ConwayLifeShould.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 public class ConwayLifeShould {
 
+
     @Test public void
     satisfact_rule_number_1(){
         int[][][] gliders = {
@@ -29,5 +30,20 @@ public class ConwayLifeShould {
         assertTrue("Got \n" + LifeDebug.htmlize(res) + "\ninstead of\n" + LifeDebug.htmlize(gliders[1]), LifeDebug.equals(res, gliders[1]));
 
     }
+
+    @Test public void
+    satisfact_rule_number_3(){
+        int[][][] gliders = {
+                {{0,0,0}, {0,1,0}, {1,1,1}},
+                {{0,0,0}, {0,1,0}, {1,1,1}}
+        };
+        System.out.println("Glider");
+        LifeDebug.print(gliders[0]);
+        int[][] res = ConwayLife.getGeneration(gliders[0], 1);
+        assertTrue("Got \n" + LifeDebug.htmlize(res) + "\ninstead of\n" + LifeDebug.htmlize(gliders[1]), LifeDebug.equals(res, gliders[1]));
+
+    }
+
+
 
 }
